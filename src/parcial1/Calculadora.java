2a7cd98787676da4a8fff759a9b7bd8f4dfd1367
Estudiantes <Estudiantes@labing50212.udistrital.edu.co>
2017-03-24 17:10:20 -0500

    parte 1

---
+++ src/parcial1/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial1;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+    int num1,num2,resultado;
+    
+    public float suma(){
+        
+        resultado=num1+num2;
+        
+        return resultado;
+    }
+}
